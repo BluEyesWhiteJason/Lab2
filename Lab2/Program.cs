776076c95f9acb76a867bb5a8aec36991978ea35
BluEyesWhiteJason <jason.speet@gmail.com>
2019-07-10 20:04:09 -0400

    first commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cont = true;
+            while (cont == true)
+            {
+                Console.WriteLine("Please enter the width of the room: ");
+                double width = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter the length of the room: ");
+                double length = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter the height of the room: ");
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                double area = width * length;
+                double perim = (width * 2) + (length * 2);
+                double volume = width * length * height;
+
+                Console.WriteLine("The area is " + area);
+                Console.WriteLine("The perimeter is " + perim);
+                Console.WriteLine("The volume is " + volume);
+
+                Console.WriteLine("Continue? y/n : ");
+                string contEnter = Console.ReadLine();
+
+                if (contEnter == "y")
+                {
+                    cont = true;
+                }
+                else if (contEnter == "n")
+                {
+                    cont = false;
+                }
+
+
+            }
+           
+        }
+    }
+}
